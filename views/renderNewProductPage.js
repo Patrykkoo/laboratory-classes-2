60c4d005bb158d0bbc52e6184d187d7e364db606
Patrykkoo <patryk.chojnacki02@gmail.com>
2025-04-02 14:53:14 +0200

    Zadanie z renderNewProductPage.js

--- views/renderNewProductPage.js
+++ views/renderNewProductPage.js
@@ -1,5 +1,30 @@
-/* 🏗 Structo the Builder */
-/* Funkcja generująca stronę HTML z najnowszym produktem */
-const renderNewProductPage = () => {};
+const fs = require("fs");
+const path = require("path");
+
+const renderNewProductPage = (response) => {
+  fs.readFile(path.join(__dirname, "../product.txt"), "utf-8", (err, data) => {
+    response.setHeader("Content-Type", "text/html");
+
+    response.write("<html>");
+    response.write("<head><title>Shop - Newest product</title></head>");
+    response.write("<body>");
+    response.write("<h1>Newest product</h1>");
+    response.write(
+      "<nav><a href='/'>Home</a><br /><a href='/product/add'>Add product</a><br /><a href='/logout'>Logout</a></nav>"
+    );
+
+    if (err || !data || data.trim() === "") {
+      response.write("<br /><div>No new products available.</div>");
+    } else {
+      const splittedData = data.split("\n").join("<br />");
+      response.write(`<br /><div>New product data - ${splittedData}</div>`);
+    }
+
+    response.write("</body>");
+    response.write("</html>");
+
+    return response.end();
+  });
+};
 
 module.exports = renderNewProductPage;
