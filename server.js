bf4c676cf4cb8757c218a1f6c7757515492a2097
Patrykkoo <patryk.chojnacki02@gmail.com>
2025-04-02 14:06:16 +0200

    Zadanie z server.js

--- server.js
+++ server.js
@@ -1,40 +1,35 @@
-/*
-  📦 Dependy the Importer  
-  Zaimportuj wszystkie wymagane moduły: path, express, body-parser, logger oraz routing.  
-*/
-const http = require("http");
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { getInfoLog, getErrorLog } = require("./utils/logger");
+const productRoutes = require("./routing/product");
+const logoutRoutes = require("./routing/logout");
+const killRoutes = require("./routing/kill");
+const homeRoutes = require("./routing/home");
+const { STATUS_CODE } = require("./constants/statusCode");
 const config = require("./config");
-const { requestRouting } = require("./routing/routing");
 
-const requestListener = (request, response) => {
-  requestRouting(request, response);
-};
+const app = express();
 
-const server = http.createServer(requestListener);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-server.listen(config.PORT);
+app.use((req, res, next) => {
+  getInfoLog(req.method, req.url);
+  next();
+});
 
-/*
-  🏗 Structo the Builder  
-  Utwórz instancję aplikacji express i zapisz ją w stałej app.  
-*/
-/*
-  🏗 Structo the Builder  
-  Zarejestruj middleware body-parser do parsowania ciał formularzy. 
-*/
-/*
-  🏗 Structo the Builder  
-  Dodaj middleware logujący informacje o każdym przychodzącym żądaniu.  
-*/
-/*
-  🏗 Structo the Builder  
-  Zarejestruj middleware obsługujące poszczególne ścieżki.  
-*/
-/*
-  🏗 Structo the Builder  
-    Obsłuż stronę 404 – zwróć plik 404.html i zaloguj błąd.   
-*/
-/*
-  🏗 Structo the Builder  
-    Uruchom serwer i nasłuchuj na porcie z config.js.    
-*/
+app.use("/product", productRoutes);
+app.use("/logout", logoutRoutes);
+app.use("/kill", killRoutes);
+app.use("/", homeRoutes);
+
+app.use((req, res) => {
+  res.status(STATUS_CODE.NOT_FOUND);
+  res.sendFile(path.join(__dirname, "views", "404.html"));
+  getErrorLog(req.url);
+});
+
+app.listen(config.PORT, () => {
+  console.log(`Serwer nasłuchuje na porcie ${config.PORT}`);
+});
